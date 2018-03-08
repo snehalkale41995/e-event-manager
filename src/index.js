cf4823209378ba4d74084708bf7b332cfb1e110b
Mahesh Kedari <kedari.mahesh@outlook.com>
2018-03-08 17:01:48 +0530

    Primary changes for firestore integration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {HashRouter, Route, Switch} from 'react-router-dom';
-
+//import './services/datastore/Datastore';
 // Styles
 // Import Flag Icons Set
 import 'flag-icon-css/css/flag-icon.min.css';
@@ -21,6 +21,22 @@ import Login from './views/Pages/Login/'
 import Page404 from './views/Pages/Page404/'
 import Page500 from './views/Pages/Page500/'
 
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmhu_J_9kRqDuQZox7ccZVNgnOA9fc4Gw",
+  authDomain: "tie-con-management.firebaseapp.com",
+  databaseURL: "https://tie-con-management.firebaseio.com",
+  projectId: "tie-con-management",
+  storageBucket: "tie-con-management.appspot.com",
+  messagingSenderId: "852890830155"
+} // from Firebase Console
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig)
+// Initialize Cloud Firestore through Firebase
+export const firebasedb = firebase.firestore();
 
 ReactDOM.render((
   <HashRouter>
