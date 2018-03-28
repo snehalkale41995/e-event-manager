191f68503cd92b699ae490af392772e5abc7de4e
shaggyshelar <shaggy.shelar@gmail.com>
2018-03-28 18:25:53 +0530

    Added mail sending functionality

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,23 @@
 const functions = require('firebase-functions');
 var admin = require("firebase-admin");
+const nodemailer = require('nodemailer');
 var serviceAccount = require('./tie-app.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://tie-con-management.firebaseio.com'
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://tie-con-management.firebaseio.com'
 });
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: '',
+        pass: '',
+    },
+});
+
+const APP_NAME = 'TiECON Pune';
+
 exports.registerUser = functions.https.onRequest((request, response) => {
     if (request.method !== 'POST') {
         return response.status(403).send('Forbidden!');
@@ -35,12 +43,30 @@ exports.registerUser = functions.https.onRequest((request, response) => {
         displayName: request.body.displayNamed,
         disabled: false
     })
-    .then((userRecord) => {
-        console.log("Successfully created new user:", userRecord.uid);
-        return response.status(200).send("Successfully created new user:");
-    })
-    .catch((error) => {
-        console.log("Error creating new user:", error);
-        return response.status(500).send("Error creating new user:");
-    });
+        .then((userRecord) => {
+            console.log("Successfully created new user:", userRecord.uid);
+            return sendWelcomeEmail(request.body.userEmail, request.body.displayNamed, response, request.body.password);
+        })
+        .catch((error) => {
+            console.log("Error creating new user:", error);
+            return response.status(500).send("Error creating new user:");
+        });
 });
+
+function sendWelcomeEmail(email, displayName, response, password) {
+    const mailOptions = {
+      from: `${APP_NAME} <noreply@firebase.com>`,
+      to: email,
+    };
+  
+    // The user subscribed to the newsletter.
+    mailOptions.subject = `Welcome to ${APP_NAME}!`;
+    mailOptions.text = `Hey ${displayName || ''}! Thank you for your registration at ${APP_NAME}. Your password for login is '${password}'.`;
+    return mailTransport.sendMail(mailOptions).then(() => {
+      console.log('New welcome email sent to:', email);
+      return response.status(200).send("Successfully created new user");
+    }).catch((error) => {
+        console.log("Error sending mail to user:", error);
+        return response.status(500).send("Error sending mail to user:" + email);
+    });
+  }
