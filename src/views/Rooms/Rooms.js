5742346d10c5183c6c7409974fac224b3e6ea66f
Sagar Agale <sagar.agale@Eternus.local>
2018-04-05 15:57:12 +0530

    Made changes in Room module

--- src/views/Rooms/Rooms.js
+++ src/views/Rooms/Rooms.js
@@ -1,164 +1,178 @@
 import React, { Component } from 'react';
-import { Bar, Line } from 'react-chartjs-2';
 import {
-    Container, Input, InputGroup, InputGroupText, InputGroupAddon, Badge, Row, Col, Progress, Dropdown, DropdownToggle,
-    DropdownMenu, DropdownItem, Card, CardHeader, CardBody, CardFooter, CardTitle, Button, ButtonToolbar,
-    ButtonGroup, ButtonDropdown, Label, Table, Form, FormGroup, FormText,
+    Input, InputGroup, InputGroupText, InputGroupAddon, Row, Col,   
+    Card,  CardBody, Button, FormGroup
 } from 'reactstrap';
 import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+import { ToastContainer, toast } from 'react-toastify';
+
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 import { DBUtil } from '../../services';
-import 'react-select/dist/react-select.css';
-import { createBrowserHistory } from 'history';
-
-var history = createBrowserHistory();
-let ArrayService = [];
 
 class Rooms extends Component {
     constructor(props) {
         super(props);
-        var history = { history }
-        this.items = []; 
+
         this.state = {
-            Room: {
-                RoomName: '',
-                Capacity: '',
+            room: {
+                roomName: '',
+                capacity: '',
                 bufferCapacity : '',
-                AvailableServices: []
+                availableServices: []
             },
-            ServiceDropDown: [],
-            submitted: false,
-            isChecked: true
+            serviceDropDown: [],
+            submitted: false
         };
         this.changeFunction = this.changeFunction.bind(this);
         this.submitFunction = this.submitFunction.bind(this);
         this.resetField = this.resetField.bind(this);
         this.handleSelectChange = this.handleSelectChange.bind(this);   
+        this.setInputToNumeric = this.setInputToNumeric.bind(this);
     }
 
+    // Method for get defaulf services
     componentWillMount() {
         let componentRef = this;
         DBUtil.addChangeListener("AvailableServices", function (objectList) {
-            let Services = [];
-            let ArrayService = [];
+            let services = [], arrayService = [], servicesArray = [];
             objectList.forEach(function (doc) {
-               Services.push(doc.data());
+                services.push(doc.data());
             });            
-            const ServicesArray = Services[0].Services;
-            ServicesArray.forEach(function (item){
-                ArrayService.push({label : item , value : item });
+            servicesArray = services[0].services;
+            servicesArray.forEach(function (item){
+                arrayService.push({label : item , value : item });
             });
-            componentRef.setState({ServiceDropDown : ArrayService})
+            componentRef.setState({serviceDropDown : arrayService})
         });
     }
 
+    // Method for set text values
     changeFunction(event) {
         const { name, value } = event.target;
-        const { Room } = this.state;
+        const { room } = this.state;
         this.setState({
-            Room: {
-                ...Room,
+            room: {
+                ...room,
                 [name]: value
             }
         });
     }
 
+    // Method for save rooms data
     submitFunction(event) {
         event.preventDefault();
         this.setState({ submitted: true });
-        const { Room } = this.state;
-       // console.log('New Room', Room)
-        if (Room.RoomName && Room.Capacity ) {
-            if(this.state.Room.AvailableServices.length > 0){
-                let length = this.state.Room.AvailableServices.length;
-                let serviceString = this.state.Room.AvailableServices[length - 1]
+        const { room } = this.state;
+        if (room.roomName && room.capacity ) {
+            if(this.state.room.availableServices.length > 0){
+                let length = this.state.room.availableServices.length;
+                let serviceString = this.state.room.availableServices[length - 1]
                 if(serviceString == ""){
-                    this.state.Room.AvailableServices = [];
+                    this.state.room.availableServices = [];
                 }
                 else{
                     let serviceArray = serviceString.split(',');
-                    this.state.Room.AvailableServices = serviceArray;
+                    this.state.room.availableServices = serviceArray;
                 }
             } 
-            //parameters to add doc function
+            
             let componentRef = this;
             let tableName = "Rooms";
-            let docName = Room.RoomName;
+            let docName = room.roomName;
             let doc= {
-                RoomName: Room.RoomName,
-                Capacity: Room.Capacity,
-                bufferCapacity: Room.bufferCapacity,
-                AvailableServices: Room.AvailableServices
+                roomName: room.roomName,
+                capacity: room.capacity,
+                bufferCapacity: room.bufferCapacity,
+                availableServices: room.availableServices
             }
-            DBUtil.addDoc(tableName, docName, doc ,function(){          //add doc to firebase
-                console.log('added');
-                alert("Room " + docName + " successfully added" );
-                componentRef.props.history.push('/dashboard');
+            DBUtil.addDoc(tableName, docName, doc ,function(){          
+                toast.success("Room saved successfully.", {
+                    position: toast.POSITION.BOTTOM_RIGHT,
+                });
+                setTimeout(() => {
+                    componentRef.resetField(true);
+                }, 500);
             },
             function(err){
-                console.log('Error' , err);
+                toast.error("Room not saved.", {
+                    position: toast.POSITION.BOTTOM_RIGHT,
+                });
             });
-          //  this.props.history.push('/login');
         }
     }
-    resetField() {
+
+    // Method for reset all fields
+    resetField(resetFlag) {
         this.setState({
-            Room: {
-                RoomName: '',
-                Capacity: '',
+            room: {
+                roomName: '',
+                capacity: '',
                 bufferCapacity : '',
-                AvailableServices: [], 
-            },
-            isChecked: false
+                availableServices: [], 
+            }
         });
+        this.handleSelectChange(null);
+        if (resetFlag != true) {
+            toast.success("Room form reset successfully.", {
+                position: toast.POSITION.BOTTOM_RIGHT,
+            });
+        }
     }
    
+    // Method for set drop-down list
     handleSelectChange(value) {
-        this.state.Room.AvailableServices.push(value);
+        if (value != null) {
+            this.state.room.availableServices.push(value);
+        }
         this.setState({ value });
+    }
+
+    // Method for set only Numeric
+    setInputToNumeric(e) {
+        const re = /[0-9]+/g;
+        if (!re.test(e.key)) {
+        e.preventDefault();
+        }
     }
     
     render() {
-        const { Room, submitted, value ,ServiceDropDown} = this.state;   
-        const options = this.state.ServiceDropDown;
+        const { room, submitted, value ,serviceDropDown} = this.state;   
+        const options = this.state.serviceDropDown;
 
         return (
             <div className="animated fadeIn">
-                
                     <Row className="justify-content-left">
                         <Col md="8">
                             <Card className="mx-6"> 
                                 <CardBody className="p-4">
                                     <h1>Room</h1>
                                     <FormGroup row>
-                                        <Col xs="12" md="6" className={(submitted && !Room.RoomName ? ' has-error' : '')}  >
+                                        <Col xs="12" md="6" className={(submitted && !room.roomName ? ' has-error' : '')}  >
                                             <InputGroup className="mb-3">
                                                 <InputGroupAddon addonType="prepend">
                                                     <InputGroupText>
                                                         <i className="icon-home"></i>
                                                     </InputGroupText>
                                                 </InputGroupAddon>
-                                                <Input type="text" placeholder="Room Name" name="RoomName" value={this.state.Room.RoomName} onChange={this.changeFunction} />
-                                                {submitted && !Room.RoomName &&
+                                                <Input type="text" placeholder="Room Name" name="roomName" value={this.state.room.roomName} onChange={this.changeFunction} />
+                                                {submitted && !room.roomName &&
                                                         <div style={{color: "red"}} className="help-block">*Required</div>
                                                     }
                                             </InputGroup>
-                                            
                                         </Col>
-                                        <Col md="6"  >
+                                        <Col md="6">
                                             <InputGroup className="mb-3">
                                                 <InputGroupAddon addonType="prepend">
                                                     <InputGroupText><i className="icon-pie-chart"></i></InputGroupText>
                                                 </InputGroupAddon>
-                                                <Input type="number" placeholder="Capacity" name="Capacity" value={this.state.Room.Capacity} onChange={this.changeFunction} />
-                                                 {submitted && !Room.Capacity &&
+                                                <Input type="text" placeholder="Capacity" name="capacity" maxLength={15} value={this.state.room.capacity} onKeyPress={(e) => this.setInputToNumeric(e)} onChange={this.changeFunction} />
+                                                 {submitted && !room.capacity &&
                                                 <div style={{color: "red"}} className="help-block">*Required</div>
                                             }
                                             </InputGroup>
-
                                         </Col>
-                                       
                                     </FormGroup>
                                     <FormGroup row>
                                     <Col xs="12"  md="6"  >
@@ -166,10 +180,10 @@ class Rooms extends Component {
                                                 <InputGroupAddon addonType="prepend">
                                                     <InputGroupText><i className="icon-pie-chart"></i></InputGroupText>
                                                 </InputGroupAddon>
-                                                <Input type="number" placeholder="Buffer Capacity" name="bufferCapacity" value={this.state.Room.bufferCapacity} onChange={this.changeFunction} />
+                                                <Input type="text" placeholder="Buffer Capacity" name="bufferCapacity" maxLength={15} value={this.state.room.bufferCapacity} onKeyPress={(e) => this.setInputToNumeric(e)} onChange={this.changeFunction} />
                                             </InputGroup>
                                         </Col>
-                                        <Col  md="6" className={(submitted && !Room.AvailableServices ? ' has-error' : '')}>
+                                        <Col  md="6" className={(submitted && !room.availableServices ? ' has-error' : '')}>
                                             <FormGroup>
                                                 <Select
                                                     multi
@@ -187,13 +201,13 @@ class Rooms extends Component {
                                             <Button type="button" size="md" color="success" onClick={this.submitFunction} >Create Room</Button>
                                              &nbsp;&nbsp;
                                             <Button onClick={this.resetField} type="reset" size="md" color="danger" ><i className="fa fa-ban"></i> Reset</Button>
+                                            <ToastContainer autoClose={4000} />
                                         </Col>
                                     </FormGroup>
                                 </CardBody>
                             </Card>
                         </Col>
                     </Row>
-                
             </div>
         )
     }
