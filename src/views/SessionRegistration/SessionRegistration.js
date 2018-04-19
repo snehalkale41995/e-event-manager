3ac6db2323447681f2e4a10a44db3296196fdcec
Sagar Agale <sagar.agale@Eternus.local>
2018-04-19 12:08:59 +0530

    Added attendee name in session registration module.

--- src/views/SessionRegistration/SessionRegistration.js
+++ src/views/SessionRegistration/SessionRegistration.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-import { Button } from 'reactstrap';
+import { Row, Col, Card, CardBody, CardHeader, 
+    CardFooter, FormGroup,Button
+  } from 'reactstrap';
 
 import * as firebase from 'firebase';
 import 'firebase/firestore';
 import { DBUtil } from '../../services';
 import { ToastContainer, toast } from 'react-toastify';  
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import 'react-bootstrap-table/dist/react-bootstrap-table.min.css';
 
 class SessionRegistration extends Component{
     constructor(props) {
@@ -18,7 +21,7 @@ class SessionRegistration extends Component{
     }
     
     componentWillMount() {
-        this.getRegistrationResponseData();
+       this.getRegistrationResponseData();
     }
 
     // Method for get all Registration Response data 
@@ -30,7 +33,7 @@ class SessionRegistration extends Component{
             snapshot.forEach(function (doc) {
                 registrationList.push({                    
                     id: doc.id,
-                    attendeeId:  doc.data().attendeeId,
+                    fullName: Object.keys(doc.data().attendee).length != 0 ?  doc.data().attendee.firstName + ' ' + doc.data().attendee.lastName : '',
                     sessionName: doc.data().session != undefined ? doc.data().session.eventName : ''
                 });
             });  
@@ -57,15 +60,47 @@ class SessionRegistration extends Component{
     }
 
     render(){
+        // Define constant for sorting
+        const options = {
+            sizePerPageList: [{
+                text: '250', value: 250
+                },{
+                text: '500', value: 500
+                },{
+                text: '1000', value: 1000
+                }, {
+                text: 'All', value: this.state.response.length
+                } ], // you can change the dropdown list for size per page
+                sizePerPage: 250,  // which size per page you want to locate as default
+        };
+
         return (
-            <div>
-                <BootstrapTable ref='table' data={this.state.response} pagination={true} search={true}>
-                    <TableHeaderColumn dataField='id' headerAlign='left' isKey hidden>Id</TableHeaderColumn>
-                    <TableHeaderColumn dataField='attendeeId' headerAlign='left' width='300'>Attendee Id</TableHeaderColumn>
-                    <TableHeaderColumn dataField='sessionName' headerAlign='left' width='500' >Session Name</TableHeaderColumn>
-                    <TableHeaderColumn dataField='delete' dataFormat={this.ondeleteRegistration.bind(this)} headerAlign='left'>Delete</TableHeaderColumn>
-                </BootstrapTable>
-                <ToastContainer autoClose={2000} />
+
+            <div className="animated fadeIn">
+                <Row>
+                    <Col xs="12" lg="12">
+                        <Card>
+                            <CardHeader>
+                                <FormGroup row className="marginBottomZero">
+                                        <Col xs="12" md="9">
+                                            <h1 className="regHeading paddingTop8">Session Registration List</h1>
+                                        </Col>
+                                </FormGroup>
+                            </CardHeader>
+                            <CardBody>
+                                <div>
+                                     <BootstrapTable ref='table' data={this.state.response} pagination={true} search={true} options={options}>
+                                         <TableHeaderColumn dataField='id' headerAlign='left' isKey hidden>Id</TableHeaderColumn>
+                                         <TableHeaderColumn dataField='fullName' headerAlign='left' width='200'>Name</TableHeaderColumn>
+                                         <TableHeaderColumn dataField='sessionName' headerAlign='left' width='300'>Session Name</TableHeaderColumn>
+                                         <TableHeaderColumn dataField='delete' dataFormat={this.ondeleteRegistration.bind(this)} headerAlign='left' width='100'>Delete</TableHeaderColumn>
+                                     </BootstrapTable>
+                                     <ToastContainer autoClose={2000} />
+                                </div>  
+                            </CardBody> 
+                        </Card>
+                    </Col>   
+                </Row>
             </div>
         );
     }
